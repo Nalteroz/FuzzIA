29e3aca41b43177cc64699e339ca7dc0a6505690
Allan Pimentel <nalteroz@gmail.com>
2018-03-01 22:18:57 -0300

    Criação do Evento iniciado

--- Assets/Resources/Scripts/BetAlgoritm/Event.cs
+++ Assets/Resources/Scripts/BetAlgoritm/Event.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Event
 {
@@ -15,24 +16,36 @@ public class Event
     public void SetPossibilitiesByCombination()
     {
         float DomainCount = FuzzyController.ImputDomainsList.Count;
-        List<Possibilitie> CurrentPossibilities = new List<Possibilitie>();
+        List<InputDomain> DomainsList = FuzzyController.ImputDomainsList;
+        List<List<Possibilitie>> CurrentPossibilities = new List<List<Possibilitie>>();
+        Dictionary<string, List<Possibilitie>> CombinationsDictionary = new Dictionary<string, List<Possibilitie>>();
 
-        
+        for(int i = 0; i < DomainCount; i++)
+        {
+            CurrentPossibilities.Add(GetPossibilities(DomainsList[i]));
+        }
+    }
+    public int nOfCombinations(int n)
+    {
+        return 2 ^ n;
+    }
+    public int Factorial(int n)
+    {
+        if (n == 0) return 1;
+        else if (n > 0) return n * Factorial(n - 1);
+        else throw new System.ArgumentException("Erro in event: Invalid number on factorial.");
     }
 
-    public List<Possibilitie> GetCombinations(InputDomain Domain1, InputDomain Domain2)
+    public List<Possibilitie> GetPossibilities(InputDomain domain)
     {
         List<Possibilitie> Result = new List<Possibilitie>();
-        for(int Set1Idx = 0; Set1Idx < Domain1.Sets.Count; Set1Idx++)
+        for(int SetIdx = 0; SetIdx < domain.Sets.Count; SetIdx++)
         {
-            for(int Set2Idx = 0; Set2Idx < Domain2.Sets.Count; Set2Idx++)
-            {
-                Result.Add(new Possibilitie(Domain1.Sets[Set1Idx], Domain2.Sets[Set2Idx]));
-            }
+            Result.Add(new Possibilitie(domain.Sets[SetIdx]));
         }
         return Result;
     }
-    public List<Possibilitie> GetCombinations(InputDomain Domain, List<Possibilitie> Possibilities)
+    public List<Possibilitie> GetPossibilities(InputDomain Domain, List<Possibilitie> Possibilities)
     {
         List<Possibilitie> Result = new List<Possibilitie>();
         for(int SetIdx = 0; SetIdx < Domain.Sets.Count; SetIdx++)
@@ -63,5 +76,58 @@ public class Possibilitie
     {
         ParametersList = new List<RuleParameter> { new RuleParameter(set) };
         ParametersList.AddRange(possibilitie.ParametersList);
+    }
+}
+
+public class Binary
+{
+    private int Value;
+    private string Representation;
+
+    public Binary(int value)
+    {
+        Value = value;
+        Representation = Convert.ToString(value, 2);
+    }
+    public Binary(string representation)
+    {
+        Representation = representation;
+        Value = Convert.ToInt32(representation, 2);
+    }
+
+    public static implicit operator Binary(string b)
+    {
+        return new Binary(b);
+    }
+    public static implicit operator Binary(int b)
+    {
+        return new Binary(b);
+    }
+
+    public static explicit operator int(Binary b)
+    {
+        return b.Value;
+    }
+    public static explicit operator string(Binary b)
+    {
+        return b.Representation;
+    }
+
+    public static Binary operator +(Binary a, Binary b)
+    {
+        return new Binary(a.Value + b.Value);
+    }
+    public static Binary operator +(Binary a, int b)
+    {
+        return new Binary(a.Value + b);
+    }
+    public static Binary operator -(Binary a, int b)
+    {
+        return new Binary(a.Value - b);
+    }
+
+    public static Binary operator -(Binary a, Binary b)
+    {
+        return new Binary(a.Value - b.Value);
     }
 }
