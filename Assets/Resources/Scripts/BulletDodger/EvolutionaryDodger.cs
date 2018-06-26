66f8dbd4dba4b2e6aa88f0fc0c9308318cbf4e9a
Allan Pimentel <nalteroz@gmail.com>
2018-06-26 15:30:01 -0300

    Balldodger incompleto.

--- Assets/Resources/Scripts/BulletDodger/EvolutionaryDodger.cs
+++ Assets/Resources/Scripts/BulletDodger/EvolutionaryDodger.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EvolutionaryPlayer : PlayerMovment
+public class EvolutionaryDodger : PlayerMovment
 {
+    public float SightDistance = 1;
+
     FuzzyController FzCtrl;
     InputDomain Input1, Input2;
     OutputDomain Output;
     Game GameCtlr;
+
     float Defused, NullValue = float.NegativeInfinity;
+    GameObject AimBullet;
 
     void Start ()
     {
@@ -17,9 +21,9 @@ public class EvolutionaryPlayer : PlayerMovment
         Input2 = FzCtrl.AddInputDomain("Orientation", new Range(-1, 1));
         Output = FzCtrl.AddOutputDomain("Move", new Range(-1, 1));
 
-        Input1.AddSet("Close", new float[] {0, Distance/4, Distance/2});
-        Input1.AddSet("Middle", new float[] {Distance/4, Distance/2, (3*Distance)/4 });
-        Input1.AddSet("Far", new float[] { Distance/2, (3*Distance)/4,  Distance});
+        Input1.AddSet("Close", new float[] {0, SightDistance / 4, SightDistance / 2});
+        Input1.AddSet("Middle", new float[] { SightDistance / 4, SightDistance / 2, (3* SightDistance) /4 });
+        Input1.AddSet("Far", new float[] { SightDistance / 2, (3* SightDistance) /4, SightDistance });
 
         Input2.AddSet("Right", new float[] { -1, -1, 0});
         Input2.AddSet("Mid", new float[] { -0.5f, -0, 0.5f });
@@ -27,10 +31,21 @@ public class EvolutionaryPlayer : PlayerMovment
 
         Output.AddSet("Left", new float[] { -1, -1, 0});
         Output.AddSet("Right", new float[] { 0, 1, 1});
+
     }
 	
 	void Update ()
     {
 
 	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        AimBullet = collision.gameObject;
+    }
+
+    float CheckDistance(GameObject Bullet)
+    {
+        return (transform.position - Bullet.transform.position).magnitude;
+    }
 }
---
+++ Assets/Resources/Scripts/Test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+    Matrix4x4 M = Matrix4x4.zero;
+    Vector2 v = Vector2.up, vt;
+	// Use this for initialization
+	void Start () {
+        M.m01 = 1;
+        M.m10 = 1;
+
+        vt = M.MultiplyVector(v);
+        Debug.Log(v);
+        Debug.Log(M);
+        Debug.Log(vt);
+	}
+	
+	
+}
