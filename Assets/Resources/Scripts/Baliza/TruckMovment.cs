b37c586cbffff02b1cb8dbf1ecf9daed8e02e3a0
Allan Pimentel <nalteroz@gmail.com>
2018-07-05 13:25:22 -0300

    Inicio da implementação Exemplo baliza

---
+++ Assets/Resources/Scripts/Baliza/TruckMovment.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TruckMovment : MonoBehaviour
+{
+    public float Velocity = 4, TunrVelocity = 4;
+
+
+	void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+        MoveFoward(1);
+	}
+    
+
+    public void MoveFoward(int Orientation)
+    {
+        transform.position += transform.up * Orientation * Velocity * Time.deltaTime;
+    }
+
+    public void Turn(int Orientation)
+    {
+        transform.Rotate(new Vector3(0, 0, 1 * TunrVelocity * Orientation));
+    }
+}
